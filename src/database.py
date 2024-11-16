fb546d2a297fa7459a1d17c04f04fa43fdbc2dc2
Александра <aleksandrasilina@gmail.com>
2024-11-16 23:57:59 +0300

    change database.py

--- src/database.py
+++ src/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from settings import db_name, host, password, port, user
+from src.settings import db_name, host, password, port, user
 
 engine = create_engine(
     url=f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{db_name}",
